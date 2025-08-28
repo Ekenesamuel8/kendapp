c3febf20db918ad5ab8ec02cbe7e7c1a730954d1
ekenesamuel <chikwenduekene8@gmail.com>
2025-08-28 11:18:21 +0100

    allow host

--- backend/veri_hut_backend/veri_hut_backend/settings.py
+++ backend/veri_hut_backend/veri_hut_backend/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-v0jmy1lj426!-v8_gacd(st!zii@_sb&f!qsl%tz*mg+vzi&4f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*'
+]
 
 
 # Application definition
